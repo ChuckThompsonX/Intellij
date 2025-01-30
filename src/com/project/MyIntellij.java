33fd5af4145183c1d0ba2bb000329b76e7f17f5f
Chuck Thompson <ChuckThompsonX@gmail.com>
2025-01-30 09:27:05 -0800

    Initial project

---
+++ src/com/project/MyIntellij.java
@@ -0,0 +1,8 @@
+package com.project;
+
+public class MyIntellij {
+    public static void main(String[] args) {
+        System.out.println("My Intellij!");
+        System.out.println("Integrated with GitHub!");
+    }
+}
